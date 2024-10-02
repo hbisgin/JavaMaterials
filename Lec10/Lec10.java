f87ba08870d32d711fc1e782dfd42a09944981ef
Halil Bisgin <halilbisgin@gmail.com>
2024-10-02 13:52:09 -0400

    Add files via upload
    Lec 10 added

---
+++ Lec10/Lec10.java
@@ -0,0 +1,14 @@
+
+public class Lec10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		for(;i<10;i++)
+		{
+			System.out.println("Hi " + i);
+		}
+
+	}
+
+}
