e2b4c5b93202d92addbe9f7a17cad4fc1a2502a3
Halil Bisgin <halilbisgin@gmail.com>
2024-10-08 13:20:23 -0400

    added untracked

---
+++ Lec10/Lec10.java
@@ -0,0 +1,14 @@
+
+public class Lec10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0; //initialization took place here and it's safe to use this in the for header below
+		for(;i<10;i++)
+		{
+			System.out.println("Hi " + i);
+		}
+
+	}
+
+}
