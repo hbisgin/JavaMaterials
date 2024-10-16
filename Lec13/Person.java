76ce3b106bf83409462caa18eecdca775f47df7a
Halil Bisgin <halilbisgin@gmail.com>
2024-10-16 13:40:21 -0400

    after alist

--- Lec13/Person.java
+++ Lec13/Person.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Person {
 
@@ -16,7 +17,27 @@ public class Person {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<String> sArray = new ArrayList<String>(); 
+		System.out.println("Arguments " + args[0] + " " + args[1] + " "+ args[2]);
 		System.out.println("Average is " + getAverage(3.5, 7.5, 1));
+		
+		sArray.add("Adam");
+		sArray.add("Eve");
+		sArray.add("Joe");
+		sArray.add("Jane");
+		sArray.add("Mary");
+		
+		for(int i=0;i<sArray.size();i++)
+		{
+			System.out.println(sArray.get(i));
+		}
+		
+		System.out.println("******************************");
+		sArray.add(0, "Sue");
+		for(int i=0;i<sArray.size();i++)
+		{
+			System.out.println(sArray.get(i));
+		}
 
 	}
 
