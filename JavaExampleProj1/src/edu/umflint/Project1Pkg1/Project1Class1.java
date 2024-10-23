245f78fa269fbe952d9df5857f4b3631724dcd26
halil <bisgin@umich.edu>
2024-10-22 23:46:33 -0400

    sample projects

---
+++ JavaExampleProj1/src/edu/umflint/Project1Pkg1/Project1Class1.java
@@ -0,0 +1,4 @@
+package edu.umflint.Project1Pkg1;
+public class Project1Class1 {
+   public int class1var1;
+}
---
+++ JavaExampleProj2/src/edu/umflint/Project2Pkg1/Project2Class1.java
@@ -0,0 +1,8 @@
+package edu.umflint.Project2Pkg1;
+import edu.umflint.Project1Pkg1.*;
+public class Project2Class1 {
+  public static void main(String[] argv) {
+    Project1Class1 obj1 = new Project1Class1();
+    System.out.println("package works");
+  }
+}
