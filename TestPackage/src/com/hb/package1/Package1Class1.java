e73fba5460f0e70b75195a92dd2c9301c0d02f96
Halil Bisgin <halilbisgin@gmail.com>
2024-10-23 12:26:31 -0400

    Testpackage

---
+++ TestPackage/src/com/hb/package1/Package1Class1.java
@@ -0,0 +1,5 @@
+package com.hb.package1;
+
+public class Package1Class1 {
+	public int class1Var1;
+}
---
+++ TestPackage/src/com/hb/package2/Package2Class1.java
@@ -0,0 +1,13 @@
+package com.hb.package2;
+
+import com.hb.package1.*;
+public class Package2Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Package1Class1 obj = new Package1Class1();
+		System.out.println(obj.class1Var1);
+
+	}
+
+}
