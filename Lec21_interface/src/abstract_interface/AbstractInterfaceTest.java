5d1ba7b9a364726548c811c01ae2e5e9c48defc3
Halil Bisgin <halilbisgin@gmail.com>
2024-11-18 13:27:26 -0500

    interface example

--- Lec21_interface/src/abstract_interface/AbstractInterfaceTest.java
+++ Lec21_interface/src/abstract_interface/AbstractInterfaceTest.java
@@ -7,6 +7,23 @@ public class AbstractInterfaceTest {
 		CBB cbb1 = new CBB();
 		cbb1.displayDetails();
 		cbb1.play();
+		Player cbb2 = new CBB();
+		Student cbb3 = new CBB(); //can you call the play method?
+		//cbb3.play();//play method is from the Play interface
+		
+		Person cbb4 = new CBB();
+		cbb2.play(); //can you call displayDetails?
+		//cbb2.displayDetails(); //this is not possible
+		
+		Person persons [] = new Person [4]; //even if you use this you're limited with the common methods
+		persons[0] = new Student();
+		persons[1] = new CBB(); 
+		
+		for(Person person: persons)
+		{
+			person.displayDetails();
+			//person.play(); //method doesn't exist in the Person class!!!!
+		}
 
 	}
 
