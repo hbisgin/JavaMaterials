2abc328b8c224d8065ae84a5bcab9e2c6c4a1f3d
Halil Bisgin <halilbisgin@gmail.com>
2024-11-25 14:09:54 -0500

    Lab 22 on custom eception class

--- Lab22/src/MPGcalculator.java
+++ Lab22/src/MPGcalculator.java
@@ -18,19 +18,45 @@ class NegativeValueException extends Exception
 }
 
 
+
 public class MPGcalculator {
+	
+	public static int CalculateMPG(int miles, int galons) throws DivisionByZeroException, NegativeValueException
+	{
+		if(miles<0 || galons<0)
+		{
+			throw new NegativeValueException("no negative entries please");
+		}
+	
+		if(galons==0)
+		{
+			throw new DivisionByZeroException("non zero galons please");
+		}
+		
+		return miles/galons; //this throws the division exception by itself
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int miles, galons;
 		Scanner input = new Scanner(System.in);
 		System.out.println("How many miles did you travel?");
-		miles = input.nextInt();
+		miles = input.nextInt(); //Assume that inputs are numbers. I rely on the user that they don't put characters.
 		System.out.println("How much gas did you consume?");
 		galons = input.nextInt();
 		
-		System.out.println("Your mpg is " + miles/galons);
-	
+		try {
+			int mpg = CalculateMPG(miles, galons);
+			System.out.println("Your mpg is " + mpg);
+		}
+		catch (DivisionByZeroException e) {
+			System.err.println(e);
+		}catch(NegativeValueException e) {
+			System.err.println(e);
+		}finally{
+			System.out.println("I hope you had a good experience with MPG calculator ");
+		}
+			
 
 	}
 
