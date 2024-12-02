475c564eb80e21ccad8ff7acb43c097991c10628
Halil Bisgin <halilbisgin@gmail.com>
2024-12-02 13:48:07 -0500

    GUI stuff

--- Lec24_GUI_part1/src/MyFrame.java
+++ Lec24_GUI_part1/src/MyFrame.java
@@ -1,9 +1,13 @@
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 
 public class MyFrame { //extends JFrame
 
@@ -13,22 +17,33 @@ public class MyFrame { //extends JFrame
 		JPanel eastPanel = new JPanel();
 		JPanel westPanel = new JPanel();
 		westPanel.setLayout(new BoxLayout(westPanel, BoxLayout.Y_AXIS));
+		//layout = new GridLayout(2,1); eastPanel.setLayout(layout);
+		eastPanel.setLayout(new GridLayout(3,1));
+		
+		JLabel scoreLabel = new JLabel("Score Board");
 		
 		JButton east1Button = new JButton("East Button 1");
 		JButton west1Button = new JButton("West Button 1");
 		
 		JButton east2Button = new JButton("East Button 2");
 		JButton west2Button = new JButton("West Button 2");
+		ImageIcon um = new ImageIcon("UM.png");
+		
+		JButton umButton = new JButton(um);
+		JButton osuButton = new JButton(new ImageIcon("OSU.png"));
 		
 		//there is no connection between panel and the frame yet!
 		
 		frame.add(eastPanel, BorderLayout.EAST);
 		frame.add(westPanel, BorderLayout.WEST);
+		frame.add(scoreLabel);
 		
 		eastPanel.add(east1Button);
 		westPanel.add(west1Button);
 		eastPanel.add(east2Button);
 		westPanel.add(west2Button);
+		eastPanel.add(umButton);
+		westPanel.add(osuButton);
 		
 		frame.setSize(400, 300);
 		frame.setVisible(true);
